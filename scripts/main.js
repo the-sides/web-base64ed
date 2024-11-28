37e0b5891a5788efb891a345e6d52f06090a3434
Jacob Sides <sides798@gmail.com>
2024-11-28 12:55:22 -0500

    Works by searching for '=='

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+document.body.querySelectorAll('p,a,span,h6,h5,h4,h3,h2,h1,p *,a *,span *,h6 *,h5 *,h4 *,h3>*,h2 *,h1 *').forEach(elm => {
+    if(elm?.innerText?.includes('==')) {
+        if(elm.children.length > 0 && [...elm.children].some(el => el.innerText?.includes('=='))) return;
+        
+        const words = elm.innerText.split(' ')
+        words.forEach((word, i) => {
+            const isBase = word.at(-1) === '=' && word.at(-2) === '=';
+            if(isBase){
+                words[i] = atob(word)
+            }
+        })
+        elm.innerText = words.join(' ')
+    }
+})
